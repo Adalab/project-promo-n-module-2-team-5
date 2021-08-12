b86a5cb751b348fc097e98b3d70ded5c9ad78bc8
Clara Vilela <claravilelajato@gmail.com>
2021-08-12 13:25:58 +0200

    Correct arrays

---
+++ src/js/01 collapsable.js
@@ -0,0 +1,19 @@
+"use strict";
+//const collapseDivHTML = document.querySelector(".js-collapse");
+//const rotateaArrow = document.querySelector(".js-arrow");
+const titleToclickList = document.querySelectorAll(".collapsible-title");
+/* const clickEvent = titleToclick.addEventListener('click', closeSection); */
+function closeSection(event) {
+  const collapseDivHTML = event.currentTarget.querySelector(".js-collapse");
+  const rotateaArrow = event.currentTarget.querySelector(".js-arrow");
+  collapseDivHTML.classList.toggle("hidden");
+  rotateaArrow.classList.toggle("rotate");
+}
+
+//const afterClick = function openSection() {
+//collapseDivHTML.classList.remove('hidden');
+//};
+
+for (const titleToclick of titleToclickList) {
+  titleToclick.addEventListener("click", closeSection);
+}
