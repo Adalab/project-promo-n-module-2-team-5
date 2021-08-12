2caca815b1e28fd15dea9b002bfffc0e6b118788
monipeny <mlamasrial@gmail.com>
2021-08-12 21:57:51 +0200

    npm run docs

---
+++ docs/assets/js/main.js
@@ -0,0 +1,1 @@
+"use strict";const titleToclickList=document.querySelectorAll(".collapsible-title");function closeSection(t){const e=t.currentTarget.querySelector(".js-collapse"),c=t.currentTarget.querySelector(".js-arrow");e.classList.toggle("hidden"),c.classList.toggle("rotate")}for(const t of titleToclickList)t.addEventListener("click",closeSection);
