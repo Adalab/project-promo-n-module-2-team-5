0248b4bf3914def315499c996fdf0f98a33e7424
Clara Vilela <claravilelajato@gmail.com>
2021-09-01 13:59:45 +0200

    changes on rama-Clara

--- src/js/01 collapsable.js
+++ src/js/01 collapsable.js
@@ -3,14 +3,16 @@
 //const rotateaArrow = document.querySelector(".js-arrow");
 //seleccionamos dentro de todo el documento, los elementos HTML con
 //los que trabajamos, en este caso todas las secciones con clase collapsible-title
-const titleToclickList = document.querySelectorAll('.collapsible-title');
-//definimos una funcion para el listener, que se ejecutará al hacer click, event es un objeto del navegador, usamos event.currenttarget para seleccionar clases dentro de la función, sin tener que usar document, que solo selecciona el primer elemento con esa clase. Es decir estamos usando un mismo controlador de eventos para varios elementos
-function closeSection(event) {
-  const collapseDivHTML = event.currentTarget.querySelector('.js-collapse');
+const titleToclickList = document.querySelectorAll('.js-autentic-title');
+//definimos una funcion para el listener, que se ejecutará al hacer click, event es un objeto del navegador, usamos event.currentTarget para seleccionar clases dentro de la función, sin tener que usar document, que solo selecciona el primer elemento con esa clase. Es decir estamos usando un mismo controlador de eventos para varios elementos
+const closeSection = function (event) {
+  console.log('saber donde se pulsa', event.target);
+  console.log('saber lo que se escucha', event.currentTarget);
+  let collapseDivHTML = event.target.querySelector('.js-collapse');
   const rotateaArrow = event.currentTarget.querySelector('.js-arrow');
   collapseDivHTML.classList.toggle('hidden');
   rotateaArrow.classList.toggle('rotate');
-}
+};
 
 //const afterClick = function openSection() {
 //collapseDivHTML.classList.remove('hidden');
