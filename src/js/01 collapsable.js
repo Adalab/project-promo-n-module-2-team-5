9a8b5ae31fb7ac6f7584c8b63467aa71c64b8ac6
MaitaneAbad <maitaneabadconde@gmail.com>
2021-09-02 14:06:09 +0200

    added js text form

--- src/js/01 collapsable.js
+++ src/js/01 collapsable.js
@@ -3,6 +3,7 @@
 const titleToclickList = document.querySelectorAll('.js-headerClick');
 
 function closeSection(event) {
+  //eventpreventDefault();
   const collapseDivHTML = event.currentTarget.parentNode.querySelector('.js-collapse');
   //parentNode se utiliza para buscar elementos en la madre de donde estamos clicando
   const rotateaArrow = event.currentTarget.querySelector('.js-arrow');
