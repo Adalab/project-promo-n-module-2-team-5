b0469dca34011b30735d2f61a56ea92d13a77864
MaitaneAbad <maitaneabadconde@gmail.com>
2021-09-02 13:09:11 +0200

    add js collapse

--- src/js/01 collapsable.js
+++ src/js/01 collapsable.js
@@ -1,15 +1,16 @@
 'use strict';
 
-//const titleToclickList = document.querySelectorAll('.collapsible-title');
+const titleToclickList = document.querySelectorAll('.js-headerClick');
 
-//function closeSection(event) {
-  //const collapseDivHTML = event.currentTarget.querySelector('.js-collapse');
-  //const rotateaArrow = event.currentTarget.querySelector('.js-arrow');
-  //collapseDivHTML.classList.toggle('hidden');
-  //rotateaArrow.classList.toggle('rotate');
-//}
+function closeSection(event) {
+  const collapseDivHTML = event.currentTarget.parentNode.querySelector('.js-collapse');
+  //parentNode se utiliza para buscar elementos en la madre de donde estamos clicando
+  const rotateaArrow = event.currentTarget.querySelector('.js-arrow');
+  collapseDivHTML.classList.toggle('hidden');
+  rotateaArrow.classList.toggle('rotate');
+}
 
 
-//for (const titleToclick of titleToclickList) {
-  //titleToclick.addEventListener('click', closeSection);
-//}
+for (const titleToclick of titleToclickList) {
+  titleToclick.addEventListener('click', closeSection);
+}
