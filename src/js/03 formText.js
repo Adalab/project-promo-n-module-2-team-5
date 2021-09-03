4d6be9e56e88fa759ee558400aab26e5288c3f02
Clara Vilela <claravilelajato@gmail.com>
2021-09-03 13:54:16 +0200

    Set card

--- src/js/03 formText.js
+++ src/js/03 formText.js
@@ -1,15 +1,22 @@
-"use strict";
+'use strict';
 
-const textForm = document.querySelector(".js-textForm");
-const names = document.querySelector(".js-name");
+const textForm = document.querySelector('.js-textForm');
+const jobsCard = document.querySelector('.js-jobCard');
 const namesCard = document.querySelector('.js-nameCard');
-
-
+const gitHub = document.querySelector('.js-githubcard');
 function handleTextForm(event) {
   event.preventDefault();
-  const namesHTML = names.value;
-  namesCard.innerHTML = `${namesHTML}`;
+  const inputId = event.target.id;
+  const inputValue = event.target.value;
+  console.log(event.target.id);
+
+  if (inputId === 'job') {
+    jobsCard.innerHTML = inputValue;
+  } else if (inputId === 'name') {
+    namesCard.innerHTML = inputValue;
+  } else if (inputId === 'github') {
+    gitHub.href = inputValue;
+  }
 }
 
-
-textForm.addEventListener("keyup", handleTextForm);
+textForm.addEventListener('keyup', handleTextForm);
