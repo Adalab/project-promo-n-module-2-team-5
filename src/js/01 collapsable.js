d692c6ad3234e76dc5c9b2450433925d73dcc81e
Clara Vilela <claravilelajato@gmail.com>
2021-09-07 12:13:59 +0200

    Add hidden to not clicked content of collapsables

--- src/js/01 collapsable.js
+++ src/js/01 collapsable.js
@@ -1,4 +1,4 @@
-'use strict';
+/* 'use strict';
 
 const titleToclickList = document.querySelectorAll('.js-headerClick');
 
@@ -14,4 +14,30 @@ function closeSection(event) {
 
 for (const titleToclick of titleToclickList) {
   titleToclick.addEventListener('click', closeSection);
+}
+ */
+'use strict';
+
+const titleToclickList = document.querySelectorAll('.js-headerClick');
+for (const titleToclick of titleToclickList) {
+  titleToclick.addEventListener('click', closeSection);
+}
+
+function closeSection(event) {
+  //eventpreventDefault();
+  const rotateaArrow = event.currentTarget.querySelector('.js-arrow');
+  const collapseDivHTML =
+    event.currentTarget.parentNode.querySelector('.js-collapse');
+  //parentNode se utiliza para buscar elementos en la madre de donde estamos clicando
+  console.log(collapseDivHTML);
+  const collapsables = document.querySelectorAll('.js-collapse');
+  for (const collapsable of collapsables) {
+    console.log(collapsable);
+    if (collapseDivHTML === collapsable) {
+      collapsable.classList.toggle('hidden');
+      rotateaArrow.classList.toggle('rotate');
+    } else {
+      collapsable.classList.add('hidden');
+    }
+  }
 }
