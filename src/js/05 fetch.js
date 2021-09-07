bd99d0681f3e91dcb3821a156d0140c53a45e85c
Clara Vilela <claravilelajato@gmail.com>
2021-09-07 17:50:41 +0200

    Correct indentation on JS

--- src/js/05 fetch.js
+++ src/js/05 fetch.js
@@ -4,33 +4,31 @@ const createBtn = document.querySelector('.js_create_btn');
 const cardResultElement = document.querySelector('.js_card_result');
 
 const dataUser = {
-    photo: ,
-    palette: parseInt(document.querySelector('.js_palette:checed').value),
-    name: document.querySelector('.js_input_name').value,
-    job: document.querySelector('.js_input_job').value,
-    email: document.querySelector('.js_input_email').value,
-    phone: document.querySelector('.js_input_phone').value,
-    linkedin: document.querySelector('.js_input_linkedin').value,
-    github: document.querySelector('.js_input_github').value,
+  photo: 'data:image/png;base64,2342ba...',
+  palette: parseInt(document.querySelector('.js_palette:checked').value),
+  name: document.querySelector('.js_input_name').value,
+  job: document.querySelector('.js_input_job').value,
+  email: document.querySelector('.js_input_email').value,
+  phone: document.querySelector('.js_input_phone').value,
+  linkedin: document.querySelector('.js_input_linkedin').value,
+  github: document.querySelector('.js_input_github').value,
 };
 
 function handleCreateBtn(ev) {
-    ev.preventDefault();
-    fetch('https://awesome-profile-cards.herekuapp.com/card', {
-            method: 'POST',
-            body: JSON.stringify(dataUser),
-            headers: {
-                'Content-type': 'application/json',
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            //podemos crear un función para que pinte el enlace y botón a twitt
-            cardResultElement.innerHTML = data.cardURL;
-        })
+  ev.preventDefault();
+  fetch('https://awesome-profile-cards.herekuapp.com/card', {
+    method: 'POST',
+    body: JSON.stringify(dataUser),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      //podemos crear un función para que pinte el enlace y botón a twitt
+      cardResultElement.innerHTML = data.cardURL;
+    });
 }
-
-
 
 createBtn.addEventListener('click', handleCreateBtn);
