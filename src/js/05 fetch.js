6da957fcecb3c641f30c1dfa3aa8eccf2540755c
monipeny <mlamasrial@gmail.com>
2021-09-07 13:32:44 +0200

    add class js_input

--- src/js/05 fetch.js
+++ src/js/05 fetch.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const createBtn = document.querySelector(',js')
+
 const dataUser = {
-    photo:;
-    palette;: name: ;
-    job:;
-    email:;
-    phone:;
-    linkedin:;
-    github:;
-}
+    photo: ,
+    palette: parseInt(document.querySelector('.js_palette:checed').value),
+    name: document.querySelector('.js_input_name').value,
+    job: document.querySelector('.js_input_job').value,
+    email: document.querySelector('.js_input_email').value,
+    phone: document.querySelector('.js_input_phone').value,
+    linkedin: document.querySelector('.js_input_linkedin').value,
+    github: document.querySelector('.js_input_github').value,
+};
