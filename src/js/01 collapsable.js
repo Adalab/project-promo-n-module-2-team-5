5df21a60e713c0a004e4baa1d6ae8d4cfd323032
MaitaneAbad <maitaneabadconde@gmail.com>
2021-09-07 09:15:28 +0200

    added favicon

--- src/js/01 collapsable.js
+++ src/js/01 collapsable.js
@@ -4,13 +4,13 @@ const titleToclickList = document.querySelectorAll('.js-headerClick');
 
 function closeSection(event) {
   //eventpreventDefault();
-  const collapseDivHTML = event.currentTarget.parentNode.querySelector('.js-collapse');
+  const collapseDivHTML =
+    event.currentTarget.parentNode.querySelector('.js-collapse');
   //parentNode se utiliza para buscar elementos en la madre de donde estamos clicando
   const rotateaArrow = event.currentTarget.querySelector('.js-arrow');
   collapseDivHTML.classList.toggle('hidden');
   rotateaArrow.classList.toggle('rotate');
 }
-
 
 for (const titleToclick of titleToclickList) {
   titleToclick.addEventListener('click', closeSection);
