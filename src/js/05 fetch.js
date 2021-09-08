9e7cbf8bc962da87163caf674480f23a7df4069e
MaitaneAbad <maitaneabadconde@gmail.com>
2021-09-08 11:36:02 +0200

    change fetch

--- src/js/05 fetch.js
+++ src/js/05 fetch.js
@@ -1,34 +1,47 @@
 'use strict';
 
 const createBtn = document.querySelector('.js_create_btn');
-const cardResultElement = document.querySelector('.js_card_result');
-
+const textURL = document.querySelector('.js-url');
+const form = document.querySelector('.js-form');
+//const twitterShare = document.querySelector('.js-shareTwitter');
 const dataUser = {
-  photo: 'data:image/png;base64,2342ba...',
-  palette: parseInt(document.querySelector('.js_palette:checked').value),
-  name: document.querySelector('.js_input_name').value,
-  job: document.querySelector('.js_input_job').value,
-  email: document.querySelector('.js_input_email').value,
-  phone: document.querySelector('.js_input_phone').value,
-  linkedin: document.querySelector('.js_input_linkedin').value,
-  github: document.querySelector('.js_input_github').value,
+  palette: '',
+  name: '',
+  job: '',
+  email: '',
+  phone: '',
+  linkedin: '',
+  github: '',
+  photo: '',
 };
+
+function handlerFormData(ev) {
+  const inputId = ev.target.id;
+  const inputValue = ev.target.value;
+  console.log(inputId, inputValue);
+  dataUser[inputId] = inputValue;
+  console.log(dataUser);
+}
+form.addEventListener('change', handlerFormData);
 
 function handleCreateBtn(ev) {
   ev.preventDefault();
-  fetch('https://awesome-profile-cards.herekuapp.com/card', {
+  fetch('https://awesome-profile-cards.herokuapp.com/card', {
     method: 'POST',
     body: JSON.stringify(dataUser),
     headers: {
-      'Content-type': 'application/json',
+      'Content-Type': 'application/json',
     },
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      //podemos crear un función para que pinte el enlace y botón a twitt
-      cardResultElement.innerHTML = data.cardURL;
+      if (data.success === true) {
+        //twitterShare.classList.remove('hidden');
+        textURL.innerHTML = data.cardURL;
+      } else {
+        textURL.innerHTML =
+          'Error al crear la tarjeta, por favor revise los datos introducidos';
+      }
     });
 }
-
 createBtn.addEventListener('click', handleCreateBtn);
