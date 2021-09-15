73609189a384f20189046c4f52b68a8e723e0a45
Amirandard <am.miranda.rd@gmail.com>
2021-09-15 21:02:06 +0200

    changes

--- src/js/07 localStorage.js
+++ src/js/07 localStorage.js
@@ -23,20 +23,6 @@ function setObjectInLocalStorage() {
   localStorage.setItem('dataUser', transformObjectArray);
 }
 
-<<<<<<< HEAD
-//Buscamos info en el LocalStorage y si la hay
-function getLocalStorage() {
-  // obtengo lo que hay en el LocalStorage
-  let getLocalStorageDataUser = localStorage.getItem('dataUser');
-  console.log(getLocalStorageDataUser);
-  // miramos si son validos los datos
-  if (getLocalStorageDataUser !== null) {
-    const arrayUser = JSON.parse(getLocalStorageDataUser);
-    dataUser = arrayUser;
-
-    handlerFormData();
-
-=======
 function getObjectFromLocalStorage() {
   const localStorageDataUser = localStorage.getItem('dataUser');
   if (localStorageDataUser !== null) {
@@ -44,7 +30,6 @@ function getObjectFromLocalStorage() {
     console.log(objectArray);
     dataUser = objectArray;
     paintCard();
->>>>>>> 83d84f2845ee3d3240520091f19552f289f29b51
   }
 }
 getObjectFromLocalStorage();*/
